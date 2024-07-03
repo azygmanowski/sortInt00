cdd16b180b2210205f760cc5e4f36faa351bdee4
azy <azy@DENB0158.pmatools-de.local>
2024-07-03 14:26:12 +0200

    erste Arbeitsversion für Timo mit Kommentaren

--- sortInt00/src/sortInt00/SortIntOO.java
+++ sortInt00/src/sortInt00/SortIntOO.java
@@ -6,29 +6,37 @@ public class SortIntOO {
 	
 	static Random rn = new Random();	
 		
-	public  static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public  static void main(String[] args) {		
 		
+		//Definition der Anzahl an zu sortierenden Elementen
 		int myArrayLength = 100000;
 				
+		//leeres Array definieren, aktueller Datentyp Integer (Ganzzahlen)
 		int[] myArraySort 	= new int [myArrayLength];
 		
-		
+		//Füllen der 'Liste' mit Zufallszahlen zwischen 0 und 99
 		for (int i = 0; i < myArrayLength; i++) {
 			myArraySort[i] = rn.nextInt(0,99);			
 		}		
 		
 		
+		//Startzeit festlegen
 		long startTime = System.currentTimeMillis();
+		//Liste sortieren
 		sort(myArraySort);
+		//Stopzeit festlegen
 		long endTime = System.currentTimeMillis();
-		
+		//Ausgabe der benötigten Zeit
 		System.out.println("benötigte Zeit: " + (double)(endTime - startTime)/1000 + " Sekunden");
-		
-
 	}
 	
 	
+	
+	//Ab hier ist das eigentliche Sortieren programmiert, davor der Kram dient lediglich zum Erzeugen und Füllen der 'Liste'
+	//sowie zum Messen der Zeit (Effizienz)
+	
+	
+	//Das ist die eigentliche Methode zum Sortieren
 	public static void sort(int a [])
 	{ for (int outer = 0; outer < a.length; outer++)
 		{ int min = outer;
@@ -39,6 +47,8 @@ public class SortIntOO {
 		}		
 	}
 	
+	
+	//Methode zum Tauschen zweier Werte, wird bei Bedarf von der Sortiermethode aufgerufen
 	public static  void tausche (int[] a, int i, int j)
 	{
 		int hilf = a[i];
